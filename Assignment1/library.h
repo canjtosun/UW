73074db9c8a395408c29a8678bad234b7aaffb2a
canjtosun <55419068+canjtosun@users.noreply.github.com>
2019-10-26 00:36:39 -0700

    Update library.h

--- Assignment1/library.h
+++ Assignment1/library.h
@@ -1,4 +1,25 @@
-//Can Tosun ASsignment 1
-//library.h file
+// Can Tosun Assignment 1 Class
+// 
 
+#pragma once
+using namespace std;
 
+/*Library class interface. I did not add private, bcuz
+private is default. variables are private
+*/
+class Library
+{
+	static const int MAXBOOK = 100;
+	string arr[MAXBOOK];
+	int numberOfBook = 0;
+
+public:
+	Library(string name);
+	int GetNumberOfBook();
+	bool AddBook(const string& name);
+	bool RemoveBook(const string& name);
+	int findBookIndex(const string& name) const;
+	void ListAllBooks() const;
+	bool IsInLibrary(const string& name) const;
+	friend ostream& operator<<(ostream& out, const Library& lib);
+};
